0da7544ef987ffe0dc993b4bd94839c29cb0f0e0
m_c_w <mcwiley@comcast.net>
2019-06-23 10:49:14 -0400

    Add project files.

---
+++ TestMaker/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
